5cd902bcb126a479089361e740cc82a196ce19a0
maryline888 <maryline888@gmail.com>
2023-03-14 13:33:29 -0400

    c12 ajout des fonctions js afin de faire donctionner le carrousel

--- carrousel.php
+++ carrousel.php
@@ -1,19 +1,20 @@
 <?php
 
 /**
- * Extension carrousel permet d'afficher dans une boite modale les images d'une galerie
- * @package name : Carrousel
- * Version : 1.0.0
+ * Extension carrousel permet d'afficher dans une boîte modale 
+ * les images d'une galerie
+ * Package name : Carrousel
+ * Version: 1.0.0
  */
 /*
 Plugin Name: Carrousel
-Description: Permet d'afficher dans une boîte modale les images d'une galerie avec un système de navigationPlugin URI: https://github.com/maryline888/s3_31w/tree/carrousel
 Author: Maryline Carrier
 Author URI: https://github.com/maryline888/31w_carrousel
 Description: Permet d'afficher dans uns boite modale les images d'une galerie.
 */
 function carrousel_enqueue()
 {
+
     $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
     $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
 
